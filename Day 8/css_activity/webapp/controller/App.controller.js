1969c916276d9464dd7d7c01c4bd77823a1a8146
Stanlee John Lantano <stanlee.john.lantano@accenture.com>
2025-08-14 07:58:55 +0000

    commit

---
+++ Day 8/css_activity/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("cssactivity.controller.App", {
+      onInit() {
+      }
+  });
+});
